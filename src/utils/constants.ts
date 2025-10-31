67459213ed60b531338b3f1d255817ba523ec3bf
Chev <chevv0004@gmail.com>
2025-10-31 00:10:36 -0500

    Initial commit

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -629,6 +629,10 @@ export const Devs = /* #__PURE__*/ Object.freeze({
         name: "prism",
         id: 390884143749136386n,
     },
+    chev: {
+        name: "chev",
+        id: 559278744330698752n
+    }
 } satisfies Record<string, Dev>);
 
 // iife so #__PURE__ works correctly
